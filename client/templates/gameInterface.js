78e4702fe1d9156d3220042f1fbfd6c7f66eaf29
Joshua Crowley <joshua.crowley@gmail.com>
2015-02-08 17:34:05 +1100

    Updated

--- client/templates/gameInterface.js
+++ client/templates/gameInterface.js
@@ -1,5 +1,18 @@
 Template.gameInterface.helpers({
-    woot: function () {
-        return "woot";
+    kingdom: function () {
+        return Kingdoms.findOne({
+        	gameToken: Meteor.user().profile.currentGame,
+        	monarch : Meteor.userId()
+        });
       }
+});
+
+Template.gameInterface.events({
+	'submit form': function (event, template) {
+		event.preventDefault();
+		var bushelsToFeed = event.target.bushelsToFeed.value;
+		var seedOrder = event.target.seedOrder.value;
+		var acresOrder = event.target.acresOrder.value;
+		Meteor.call("nextYear", Meteor.user().profile.currentGame, Meteor.userId(), acresOrder, bushelsToFeed, seedOrder);
+	}
 });
--- server/methods.js
+++ server/methods.js
@@ -7,6 +7,51 @@ Meteor.methods({
 	startGame: function(gameToken){
 		Games.update({"gameToken" : gameToken}, {$set:{started : true}});
 		createKingdom(gameToken);
+	},
+
+	nextYear: function(gameToken, player, acres, feed, seed){
+
+		Kingdoms.update({
+			"gameToken": gameToken,
+			 "monarch": player
+			},
+			{ $set: {
+				nextAcres : acres,
+				nextFeed : feed,
+				nextSeed : seed
+			}
+		});
 	}
 
 });
+
+function createKingdom(gameToken){
+
+	var players = Games.findOne({"gameToken": gameToken}).players;
+	var price = _.random(1,10) + 16;
+
+	players.forEach(function (player) {
+		
+		Kingdoms.insert({
+			"gameToken" : gameToken,
+			monarch : player,
+			advice : "Everyone dies",
+			year : 1,
+			starved : 0,
+			newcomers : 5,
+			population : 100,
+			acres : 1000,
+			bushels : 2800,
+			harvest : 3,
+			rats : 200,
+			price : price,
+			nextAcres : 0,
+			nextFeed : 0,
+			nextSeed : 0,
+			totalStarved: 0
+		});
+
+		console.log("kingdom created for " + player);
+	
+	});
+};
